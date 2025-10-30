9a6f9e10923f3b9774dee8e770ab4360e2a8146a
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-10-30 14:52:05 -0300

    fix: remove app.listen and export app for Verce

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 connectDB();
-
 app.use("/api/posts", postsRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+module.exports = app;
