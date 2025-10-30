afef96210aa016f9b56796c7479ec705a59b5f6f
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-10-30 17:19:10 -0300

    feat: remove required of authorImageBytes

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema(
   {
     author: { type: String, required: true },
-    authorImageBytes: { type: String, required: true },
+    authorImageBytes: { type: String},
     content: { type: String, required: true },
     likes: { type: Number, default: 0 },
     isLiked: { type: Boolean, default: false },
